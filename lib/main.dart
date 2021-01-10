d62c32715c0a89efaf139a6a2cf2d751d7e85489
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-11 03:04:17 +0530

    App Bar and Background Color Added

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red[200],
+      appBar: AppBar(
+        backgroundColor: Colors.red[400],
+        title: Text('I Am Rich'),
+      ),
+    ),
+  ));
+}
