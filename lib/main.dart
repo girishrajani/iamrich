235a06bb8b087173c16aaeda9b9dded5deafa64c
Girish Rajani <girish.r.rajani@gmail.com>
2021-01-11 03:27:26 +0530

    Added new image and updated assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       backgroundColor: Colors.red[200],
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://raw.githubusercontent.com/girishrajani/iamrich/main/assets/750x750bb.jpeg'),
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.red[400],
         title: Text('I Am Rich'),
